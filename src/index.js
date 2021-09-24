b34634c1fefaa4d9dfd92c12237d9594775dc4e9
ojegit <ojegit@gmail.com>
2021-09-24 14:48:55 +0300

    Third and fourth

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ if (document.readyState !== "loading") {
 function initializeCode() {
   var addCommentButton = document.getElementById("add-comment");
   var removeCommentsButton = document.getElementById("remove-comments");
-  var list = document.getElementById("list");
+  var list = document.getElementById("comment-list");
+  var submitButton = document.getElementById("submit-button");
 
   // Add comment from the textarea to the list
   addCommentButton.addEventListener("click", function () {
@@ -29,5 +30,20 @@ function initializeCode() {
     if (res == true) {
       list.innerHTML = "";
     }
+  });
+
+  // Submit review
+
+  submitButton.addEventListener("click", function () {
+    var textAreaValue = document.getElementById("comment-area").value;
+    var ratingValue = document.getElementById("select-feedback");
+    var strRating = ratingValue.options[ratingValue.selectedIndex].text;
+
+    list.innerHTML +=
+      '<div class="comment"><div class="comment-rating">' +
+      strRating +
+      '</div><div class="comment-text">' +
+      textAreaValue +
+      "</div></div>";
   });
 }
